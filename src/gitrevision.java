4c36a0d1cd865137b54d695b830e74107257595b
kingsley19 <karinze@ymail.com>
2014-11-27 01:12:51 +0000

    yeah i just uploaded my array class

---
+++ src/gitrevision.java
@@ -0,0 +1,18 @@
+
+public class gitrevision {
+	
+	public static void main(String [] args){
+		
+		String[] anum = new String[233];
+		anum[34] = "John the baptist";
+		anum[21] = "Jesus christ";
+		anum[23] = "Apostle paul";
+		System.out.println(anum[34]);
+		
+		
+					
+	
+		
+}
+
+}
